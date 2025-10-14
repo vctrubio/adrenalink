ed259681c86687355ee837e0daf408e692b63826
vctrubio <vctrubio@gmail.com>
2025-10-14 23:46:14 +0200

    init school and student getters

---
+++ getters/schools-getter.ts
@@ -0,0 +1,16 @@
+import type { School } from "@/drizzle/schema";
+
+export function getSchoolName(school: School): string {
+  return school.name;
+}
+
+export function getSchoolInfo(school: School) {
+  return {
+    id: school.id,
+    name: school.name,
+    country: school.country,
+    phone: school.phone,
+    createdAt: school.createdAt,
+    updatedAt: school.updatedAt,
+  };
+}
---
+++ getters/students-getter.ts
@@ -0,0 +1,17 @@
+import type { Student } from "@/drizzle/schema";
+
+export function getStudentName(student: Student): string {
+  return student.name;
+}
+
+export function getStudentInfo(student: Student) {
+  return {
+    id: student.id,
+    name: student.name,
+    passport: student.passport,
+    country: student.country,
+    phone: student.phone,
+    createdAt: student.createdAt,
+    updatedAt: student.updatedAt,
+  };
+}
