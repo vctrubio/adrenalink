950b44b46c221f583a89829a48cebc4e65a20140
vctrubio <vctrubio@gmail.com>
2025-10-14 23:46:28 +0200

    my first hook, usePhoneClear

---
+++ src/hooks/usePhoneClear.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function usePhoneClear() {
+  const [clearPhone, setClearPhone] = useState(false);
+  
+  const triggerPhoneClear = () => {
+    setClearPhone(prev => !prev);
+  };
+  
+  return { clearPhone, triggerPhoneClear };
+}
