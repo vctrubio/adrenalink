a422a4de7392297ebf552763f09de360f1404519
vctrubio <vctrubio@gmail.com>
2025-10-15 15:57:37 +0200

    migrations-...

--- drizzle/migrations/0000_gifted_lucky_pierre.sql
+++ drizzle/migrations/0000_gifted_lucky_pierre.sql
@@ -1,23 +1,25 @@
 CREATE TABLE "school" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(255) NOT NULL,
+	"username" varchar(50) NOT NULL,
 	"country" varchar(100) NOT NULL,
 	"phone" varchar(20) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "school_username_unique" UNIQUE("username")
 );
 --> statement-breakpoint
 CREATE TABLE "school_students" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"school_id" integer NOT NULL,
-	"student_id" integer NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"school_id" uuid NOT NULL,
+	"student_id" uuid NOT NULL,
 	"description" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "student" (
-	"id" serial PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"passport" varchar(50) NOT NULL,
 	"country" varchar(100) NOT NULL,
--- drizzle/migrations/0001_wise_weapon_omega.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "school" ADD COLUMN "username" varchar(50) NOT NULL;--> statement-breakpoint
-ALTER TABLE "school" ADD CONSTRAINT "school_username_unique" UNIQUE("username");
