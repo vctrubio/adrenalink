ad369fed7aeb69a78888d7d9f7d64010fa75c988
vctrubio <vctrubio@gmail.com>
2025-10-20 23:30:36 +0200

    getschoolcount

--- getters/students-getter.ts
+++ getters/students-getter.ts
@@ -1,6 +1,11 @@
 import type { StudentType } from "@/drizzle/schema";
+import type { StudentModel } from "@/backend/models";
 
 //this will be first_name + last_name later?
 export function getStudentName(student: StudentType): string {
     return student.name;
+}
+
+export function getStudentSchoolCount(student: StudentModel): number {
+    return student.relations?.schoolStudents?.length || 0;
 }
