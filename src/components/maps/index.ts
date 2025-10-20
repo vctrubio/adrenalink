2948dac671e69b604d569a3d2af93c4b0611e427
vctrubio <vctrubio@gmail.com>
2025-10-20 18:12:59 +0200

    delete maps for now

--- src/components/maps/index.ts
+++
@@ -1,4 +0,0 @@
-export { SchoolMapDashboard } from "./SchoolMapDashboard";
-export { SchoolMap } from "./SchoolMap";
-export { SchoolFilters } from "./SchoolFilters";
-export { SchoolListView } from "./SchoolListView";
