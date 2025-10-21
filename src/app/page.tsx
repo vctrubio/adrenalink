987c3c7c652a069132405ed0970d7cbfd0d15cf8
vctrubio <vctrubio@gmail.com>
2025-10-21 16:44:00 +0200

    welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SchoolCard from "@/src/components/cards/SchoolCard";
 import { getSchools } from "@/actions/schools-action";
 import { LandingPage } from "@/src/landing/landing-page";
+import Link from "next/link";
 
 export default async function Home() {
     const result = await getSchools();
@@ -12,13 +13,16 @@ export default async function Home() {
     console.log("result.data", result.data);
     return (
         <>
-        <LandingPage />
-        <div>
-          ---- this break here ---- and count {result.data.length}
-          {result.data.map((school) => (
-            <SchoolCard key={school.schema.id} school={school} />
-          ))}
-        </div>
+            <LandingPage />
+            <div>
+                ---- this break here ---- and count {result.data.length} ----
+                <Link href="/welcome" className="text-forest-400 underline ml-8">
+                    Sign Up Now
+                </Link>
+                {result.data.map((school) => (
+                    <SchoolCard key={school.schema.id} school={school} />
+                ))}
+            </div>
         </>
     );
 }
---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,9 @@
+import { WelcomeSchoolForm } from "@/src/components/forms/WelcomeSchoolForm";
+
+export default function SchoolFormsPage() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center p-6">
+            <WelcomeSchoolForm />
+        </div>
+    );
+}
