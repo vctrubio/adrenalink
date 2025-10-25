9421b74a669b0b3d43d83b2bc0d71db81f7b3646
vctrubio <vctrubio@gmail.com>
2025-10-25 23:30:44 +0200

    comment out

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -5,7 +5,7 @@ export default function SchoolFormsPage() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center px-2 py-4 md:p-6">
             <div className="w-full max-w-5xl mx-auto">
-                <R2ConnectivityCheck />
+                {/* <R2ConnectivityCheck /> */}
                 <WelcomeSchoolForm />
             </div>
         </div>
