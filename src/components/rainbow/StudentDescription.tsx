ae14e04661e8e6186d5dad08bfc755cdadcaac50
vctrubio <vctrubio@gmail.com>
2025-11-29 19:28:26 +0100

    rainbow build

--- src/components/rainbow/StudentDescription.tsx
+++ src/components/rainbow/StudentDescription.tsx
@@ -1,7 +1,7 @@
 export const StudentDescription = () => {
     return (
         <p className="text-lg leading-relaxed text-white/90">
-            <span className="font-semibold">Miguel</span> got lucky, he's ready to start on <span className="font-bold text-yellow-300">christmas eve</span>. All students fill out a <span className="font-bold">welcome form</span>, where they provide you with personal
+            <span className="font-semibold">Miguel</span> got lucky, he&apos;s ready to start on <span className="font-bold text-yellow-300">christmas eve</span>. All students fill out a <span className="font-bold">welcome form</span>, where they provide you with personal
             details, and receive an <span className="font-semibold">invitation to the app</span>.
         </p>
     );
