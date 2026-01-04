5f196f8c4efe3a7cd9f9e39f0d01a83321b91c0f
vctrubio <vctrubio@gmail.com>
2026-01-05 00:59:41 +0100

    max-h-80

--- src/app/(admin)/classboard/ClassboardContentBoard.tsx
+++ src/app/(admin)/classboard/ClassboardContentBoard.tsx
@@ -5,7 +5,7 @@ import TeacherClassDaily from "./TeacherClassDaily";
 
 export default function ClassboardContentBoard() {
     return (
-        <div className="flex-1 p-4 overflow-hidden min-h-0 flex flex-col">
+        <div className="flex-1 p-4 overflow-y-auto min-h-0 flex flex-col max-h-[80vh] ">
             <div className="flex flex-col xl:flex-row gap-4 flex-1 min-h-0">
                 {/* Left Column: Students */}
                 <div className="w-full xl:w-[400px] flex-shrink-0 rounded-2xl overflow-hidden border border-zinc-200 dark:border-zinc-700 flex flex-col transition-all duration-300 relative bg-card/30">
