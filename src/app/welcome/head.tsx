abb2c3dc1838461560d0cbf1195063ade5451d94
vctrubio <vctrubio@gmail.com>
2026-01-13 23:51:05 +0100

    perf: add image preloads for welcome page

---
+++ src/app/welcome/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="preload" as="image" href="/ADR.webp" />
+            <link rel="preload" as="image" href="/kritaps_ungurs_unplash/forest.jpg" />
+            <link rel="preload" as="image" href="/prototypes/north-icon.png" />
+            <link rel="preload" as="image" href="/kritaps_ungurs_unplash/wave-wide.jpg" />
+            <link rel="preload" as="image" href="/kritaps_ungurs_unplash/wave.jpg" />
+        </>
+    );
+}
